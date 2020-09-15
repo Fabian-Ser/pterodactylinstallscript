48e85d5abfb5b256c2f12c9fe9e34e67173742b4
FabianS <fabian.serree@gmail.com>
2020-09-15 19:39:59 +0200

    Update node_exporter.bash

--- node_exporter.bash
+++ node_exporter.bash
@@ -5,8 +5,8 @@ tar xvf node_exporter-1.0.1.linux-amd64.tar.gz
 sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
-sudo cd /etc/systemd/system/
-wget https://raw.githubusercontent.com/Fabian-Ser/pterodactylinstallscript/master/node_exporter.service
+cd /etc/systemd/system
+wget https://raw.githubusercontent.com/Fabian-Ser/pterodactylinstallscript/master/node_exporter.service -O /etc/systemd/system/node_exporter.service
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
 sudo systemctl enable node_exporter
